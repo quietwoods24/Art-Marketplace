f31c3ac5192786c9370e71e672ab258c54bfef16
marmeladka0-0 <mmazurova35@gmail.com>
2025-10-26 17:07:13 +0200

    add final success modal window. Purchases.html left. And check figma, maybe we need to change header a bit, because now we can only see purchases when we buy smth

--- script.js
+++ script.js
@@ -8,10 +8,34 @@ function closeModal(num) {
 }
 
 function redirectToPaymentPage() {
-  window.location.href = "payment_comfirmation.html";
+  sessionStorage.setItem('referrerURL', window.location.href); //зберігти з якої сторінки переходять на оплату
+  window.location.href = "payment_confirmation.html";
 }
 
 //other
 function redirectToArtistPage() {
-  window.location.href = "artist_page.html";
+    window.location.href = "artist_page.html";
+}
+
+function openSuccessModal(){
+    document.getElementById("successModal").style.display = "flex";
+}
+
+function closeSuccessModal(){
+    document.getElementById("successModal").style.display = "none";
+    returnToReferrer();
+}
+
+function returnToReferrer() {
+    const referrerURL = sessionStorage.getItem('referrerURL');
+    if (referrerURL) {
+        sessionStorage.removeItem('referrerURL');
+        window.location.href = referrerURL;
+    } else {
+        window.location.href = 'index.html'; 
+    }
+}
+
+function redirectToPurchasePage() {
+    window.location.href = "purchases.html";
 }
